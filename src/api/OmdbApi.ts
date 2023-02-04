442ae7d20fe04b70bb20833eaad84efcbadd7d85
Tutitoos <tutitoosdev@gmail.com>
2023-02-04 02:21:58 +0100

    Fix replace axios to node-fetch

--- src/api/OmdbApi.ts
+++ src/api/OmdbApi.ts
@@ -1,18 +1,18 @@
 import type { FilmRawOmdbApi } from "../types/films.js";
 import Logger from "../utils/Logger.js";
 import environments from "../utils/loadEnvironments.js";
-import axios from "axios";
+import fetch from "node-fetch";
 
 const { omdbApiKey, omdbApiUrl } = environments;
 
 class OmdbApi {
   async getFilmById(id: string): Promise<FilmRawOmdbApi | undefined> {
     try {
-      const response = await axios.get<FilmRawOmdbApi>(
+      const response = await fetch(
         `${omdbApiUrl}?apikey=${omdbApiKey}&i=${id}`,
       );
       if (response.status !== 200) throw new Error(response.statusText);
-      const resposneData = response.data;
+      const resposneData = (await response.json()) as FilmRawOmdbApi;
 
       return resposneData;
     } catch (error: unknown) {
